1686eddbe461d59075262c8e3425472485e610a9
mvidailhet <michel.vidailhet@gmail.com>
2019-02-06 15:47:11 +0100

    it's working !

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import 'gl-ionic-background-video';
 
 import { HomePage } from './home.page';
 
@@ -18,6 +19,7 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] // here is the schema declaration to add
 })
 export class HomePageModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { defineCustomElements } from 'gl-ionic-background-video/dist/loader'; // add this line
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +11,5 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+defineCustomElements(window); // call the function here
